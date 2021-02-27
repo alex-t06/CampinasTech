3903fe94ac311543576c91d8760295f703a79603
Alex Trindade <alex.trindade@live.com>
2021-02-27 16:10:25 -0300

    Implementação estática da lista de alunos

--- AndroidProjects/Agenda/app/src/main/java/br/com/alex/agenda/MainActivity.java
+++ AndroidProjects/Agenda/app/src/main/java/br/com/alex/agenda/MainActivity.java
@@ -2,14 +2,25 @@ package br.com.alex.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Hello World!", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Cindy", "Maria"));
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
