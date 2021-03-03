31c0a389ccdf8306832e254260e12189359f4950
Alex Trindade <alex.trindade@live.com>
2021-03-03 17:19:00 -0300

    Implementação de lógica dos botões, salvar dados do formulário, melhora do fluxo do aplicativo

---
+++ AndroidProjects/Agenda/app/src/main/java/br/com/alex/agenda/dao/AlunoDAO.java
@@ -0,0 +1,18 @@
+package br.com.alex.agenda.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alex.agenda.model.Aluno;
+
+public class AlunoDAO {
+    private final static List<Aluno> alunos = new ArrayList<>();
+
+    public void salva(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    public List<Aluno> todos() {
+        return new ArrayList<>(alunos);
+    }
+}
